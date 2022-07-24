c88d5afd92bd9d8d72d7d3efdc1f7b13d12e5275
sai-ashwin-iitmod <109903336+sai-ashwin-iitmod@users.noreply.github.com>
2022-07-24 18:08:39 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,8 @@
 mkdir -p ~/.streamlit/
-
-echo "\
-[general]\n\
-email = \"21f2000619@student.onlinedegree.iitm.ac.in\"\n\
-" > ~/.streamlit/credentials.toml
-
 echo "\
 [server]\n\
 headless = true\n\
-enableCORS=false\n\
 port = $PORT\n\
+enableCORS = false\n\
+\n\
 " > ~/.streamlit/config.toml
